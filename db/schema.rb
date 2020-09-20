19288fc566b7a98ba4fb2bbedea9ebc2412b83bc
Arique <arique.aguilar@gmail.com>
2020-09-20 18:28:58 -0500

    Adds new column to bulk discounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_230916) do
+ActiveRecord::Schema.define(version: 2020_09_20_232508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_20_230916) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "item_id"
+    t.boolean "active", default: false
     t.index ["item_id"], name: "index_bulk_discounts_on_item_id"
   end
 
