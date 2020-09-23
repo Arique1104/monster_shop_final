3df5eb215ed0a03646e1a6c1ccb97baabaa0fe49
Arique <arique.aguilar@gmail.com>
2020-09-23 06:53:52 -0500

    Change in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image:
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 
-User.create!(name: "Arique", address: "123 Fake St", city: "Chicago", state: "IL", zip: "60606", email: "arique@example.com", password: "securepassword", role: 1, merchant_id: megan.id)
+# User.create!(name: "Arique", address: "123 Fake St", city: "Chicago", state: "IL", zip: "60606", email: "arique@example.com", password: "securepassword", role: 1, merchant_id: megan.id)
 # require "factory_bot_rails"
 # require "faker"
 #
