f8f46917a338c0cc790134d3fd490f6aa8379456
Arique <arique.aguilar@gmail.com>
2020-09-23 17:43:18 -0500

    Adds active record to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,12 +1,9 @@
-require "bulk_discount"
 class Cart
-  attr_reader :contents, :discounts
+  attr_reader :contents
 
   def initialize(contents)
     @contents = contents || {}
     @contents.default = 0
-    @discounts = []
-    find_discount
   end
 
   def add_item(item_id)
@@ -29,21 +26,13 @@ class Cart
 
   def grand_total
     grand_totals = 0.0
-    if @discounts.count == 0
-      @contents.each do |item_id, quantity|
-        grand_totals += Item.find(item_id).price * quantity
-      end
-    else
-      @discounts.each do |discount|
-        @contents.each do |item_id, quantity|
-          item = Item.find(item_id)
-          if discount.merchant_id == item.merchant_id
-            amount_off = (discount.percent_discount/100.0) * (quantity * item.price)
-            grand_totals += (item.price * quantity) - amount_off
-          else
-            grand_totals += item.price * quantity
-          end
-        end
+    @contents.each do |item_string, quantity|
+      item = Item.find(item_string.to_i)
+      if find_discount(item, quantity).exists?
+        amount_off = (@discount.first.percent_discount/100.0) * (quantity * item.price)
+        grand_totals += (item.price * quantity) - amount_off
+      else
+        grand_totals += item.price * quantity
       end
     end
     grand_totals
@@ -54,37 +43,15 @@ class Cart
   end
 
   def subtotal_of(item_id)
-    @item = Item.find(item_id)
-    @quantity = @contents[item_id.to_s]
-
-    if @discounts.count == 0
-      @total_amount = @quantity * @item.price
-    end
-
-    if @discounts.count != 0
-      @discounts.each do |discount|
-        if discount.merchant_id != @item.merchant_id
-          @total_amount = @quantity * @item.price
-        else discount.merchant_id == @item.merchant_id
-          amount_off = (discount.percent_discount/100.0) * (@quantity * @item.price)
-          @total_amount = (@quantity * @item.price) - amount_off
-        end
-      end
-    end
-    @total_amount
+    @contents[item_id.to_s] * Item.find(item_id).price
   end
 
   def limit_reached?(item_id)
     count_of(item_id) == Item.find(item_id).inventory
   end
 
-  def find_discount
-    @contents.each do |item, quantity|
-      @item = Item.find(item.to_i)
-      discount = BulkDiscount.find_by(merchant_id: @item.merchant.id)
-      if !discount.nil?
-        @discounts << discount if (discount.min_purchase == quantity && discount.active == true)
-      end
-    end
+  def find_discount(item, quantity)
+      @discount = BulkDiscount.where(merchant_id: item.merchant_id).where("min_purchase <= #{quantity}").order(:percent_discount).limit(1)
   end
+
 end
