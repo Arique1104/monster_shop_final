cafb85766e735293caefdf3c7fd6e5e446d2a115
Arique <arique.aguilar@gmail.com>
2020-09-23 18:57:28 -0500

    Tests that a new coupon is automatically updating the total in the cart

--- spec/features/cart/add_items_spec.rb
+++ spec/features/cart/add_items_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe "Add Items to Cart" do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+
+
     end
 
     it "I can add an item from the items show page" do
@@ -31,6 +34,33 @@ RSpec.describe "Add Items to Cart" do
 
       expect(page).to have_content("#{@giant.name} has been added to your cart!")
       expect(page).to have_content("Cart: 2")
+    end
+
+    it "Test for cart with normal quantity that adds more item to meet discount and shows new discount" do
+      @discount_1 = @megan.bulk_discounts.create!(name: "Labor Day Sale", percent_discount: 10, min_purchase: 2, active: true)
+
+      visit item_path(@ogre)
+
+      click_button 'Add to Cart'
+
+      expect(current_path).to eq(items_path)
+
+      click_on "Cart: 1"
+
+      expect(page).to have_content("Total: $20.00")
+
+      click_button "More of This!"
+
+      expect(page).to have_content("Quantity: 2")
+
+      expect(page).to have_content("Total: $36.00")
+
+      click_button "More of This!"
+
+      expect(page).to have_content("Quantity: 3")
+
+      expect(page).to have_content("Total: $54.00")
+
     end
   end
 end
